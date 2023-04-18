bc462ac5c5252cb9b6b5ac94fd55a714c513f02b
LorrellW <112707694+LorrellW@users.noreply.github.com>
2023-04-17 20:24:47 -0500

    Expression Manager (STACK)

---
+++ main(STACK).cpp
@@ -0,0 +1,23 @@
+﻿//Lorrell Winfield 
+//Assignment 3 - Binary Tree (stack)
+//4 APR 2023
+//main.cpp
+
+#include "ExpressionManager.h"
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+	cout << "    **************************" << endl;
+	cout << "    **  Expression Manager  **" << endl;
+	cout << "    **************************" << endl;
+	cout << " - - - - - - - - - - - - - - - - -  " << endl;
+	cout << endl << endl;
+	call();
+	cout << endl << endl << "    ****************" << endl;
+	cout << "    **  GoodBye!  **" << endl;
+	cout << "    ****************" << endl;
+}
